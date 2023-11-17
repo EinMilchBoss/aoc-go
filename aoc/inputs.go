7c77c77d85e530b46dae7429dd664bd61f143e3b
EinMilchBoss <christian.benezuela@gmail.com>
2023-11-17 10:46:43 +0100

    Remove unnecessary vars.

--- aoc/inputs.go
+++ aoc/inputs.go
@@ -35,6 +35,5 @@ func ReadInputs(year, day uint) (inputs Inputs, err error) {
 
 func readInput(year, day uint, filename string) (data []byte, err error) {
 	file := fmt.Sprintf("./res/year%04v/day%02v/%s", year, day, filename)
-	data, err = os.ReadFile(file)
-	return
+	return os.ReadFile(file)
 }
